43108410b6e01719cd04721b988349ef49c47523
SeiichiN <billie175@gmail.com>
2019-09-27 17:58:33 +0900

    閉じるボタン

--- chat-corner.js
+++ chat-corner.js
@@ -4,22 +4,42 @@
 //
 
 $(function() {
-	var mode;
+	var mode, htmlbox, chatcorner_closeBtn;
 	
+	htmlbox = '<div id="chat-corner">'
+		+ '<div id="chatcorner-title"></div>'
+		+ '<div id="chatcorner-body"></div>'
+		+ '</div>';
+
+	chatcorner_closeBtn = '<div id="chatcorner-closeBtn">Ｘ</div>';
+
 	mode = 'closed';
+	// $('body').append('<div id="chat-corner">わはは</div>');
+	$('body').append(htmlbox);
+	$('#chatcorner-title').text('チャット開始');
+
 	$('#chat-corner').addClass('chat-corner-close');
 
-	$('body').append('<div id="chat-corner">わはは</div>');
-	$('#chat-corner').on('click', function () {
+	$('#chatcorner-title').on('click', function () {
+		console.log(mode);
 		if (mode === 'closed') {
 			$('#chat-corner').removeClass('chat-corner-close');
 			$('#chat-corner').addClass('chat-corner-open');
+			$('#chatcorner-title').text('チャットコーナー');
+			$('#chatcorner-title').prepend(chatcorner_closeBtn);
 			mode = 'opened';
-		} else if (mode === 'opened') {
+		}
+	});
+	$('#chatcorner-closeBtn').on('click', function () {
+		console.log('mode');
+		// if (mode == 'opened') {
+			console.log('close');
 			$('#chat-corner').removeClass('chat-corner-open');
 			$('#chat-corner').addClass('chat-corner-close');
+			$('#chatcorner-title').text('チャット開始');
+			$('#chatcorner-closeBtn').remove();
 			mode = 'closed';
-		}
+		// }
 	});
 });
 
