0520ad4b023957533b1ff5eca90a8b3df0267d88
SeiichiN <billie175@gmail.com>
2019-09-27 12:43:37 +0900

    animation added.

--- chat-corner.js
+++ chat-corner.js
@@ -4,9 +4,22 @@
 //
 
 $(function() {
+	var mode;
+	
+	mode = 'closed';
+	$('#chat-corner').addClass('chat-corner-close');
+
 	$('body').append('<div id="chat-corner">わはは</div>');
 	$('#chat-corner').on('click', function () {
-		$('#chat-corner').addClass('chat-corner-open');
+		if (mode === 'closed') {
+			$('#chat-corner').removeClass('chat-corner-close');
+			$('#chat-corner').addClass('chat-corner-open');
+			mode = 'opened';
+		} else if (mode === 'opened') {
+			$('#chat-corner').removeClass('chat-corner-open');
+			$('#chat-corner').addClass('chat-corner-close');
+			mode = 'closed';
+		}
 	});
 });
 
